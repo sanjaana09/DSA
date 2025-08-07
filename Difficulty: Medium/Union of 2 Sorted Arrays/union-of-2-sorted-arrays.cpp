2601d93f01c0a2d1c1f48023bfa390b4d8129994
Sanjana Garg <158046849+sanjaana09@users.noreply.github.com>
2025-08-07 15:27:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -1,25 +1,51 @@
 class Solution {
   public:
+  
+//   vector<int> sortedArray(...)
+// This defines a function named sortedArray.
+// It returns a value of type vector<int>, which is a dynamic array (from the C++ STL).
+// This means the function will return a list of integers — most likely a sorted or unioned array.
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // code here
+        // Optimal Approach ---
         int n1 = a.size() ;
         int n2 = b.size() ;
-        set <int> st ;
-        
-        for(int i = 0 ; i<n1 ; i++)
-        {
-            st.insert(a[i]) ;
+        int i = 0 ;
+        int j = 0 ;
+        vector<int> unionArr ;
+        while(i<n1 && j < n2) {
+            if(a[i] <= b[j] ) {
+                if(unionArr.size() == 0  || unionArr.back() != a[i])
+                {
+                    unionArr.push_back(a[i]) ;
+                 }
+                i++ ;
         }
-        for(int i = 0 ; i<n2 ; i++)
-        {
-            st.insert(b[i]) ;
-        }
-        
-        vector<int> temp ;
-        for(auto it : st){
-            temp.push_back(it) ;
-        }
-        return temp ;
-        
+             else {
+                if (unionArr.size() == 0 || unionArr.back() != b[j]) 
+                {
+                    unionArr.push_back(b[j]);
+                }
+                j++ ;
+               }
+                              }
+            
+             while (j<n2)
+             {
+                if(unionArr.size() == 0  || unionArr.back() != b[j])
+                {
+                    unionArr.push_back(b[j]) ;
+                 }
+                j++ ;
+             }
+             while (i<n1)
+             {
+                if(unionArr.size() == 0  || unionArr.back() != a[i])
+                {
+                    unionArr.push_back(a[i]) ;
+                 }
+                i++ ;
+             }
+             
+             return unionArr ;
     }
 };
